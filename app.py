6f4227f6045740c7b101b8058d8903a78f748a59
godunguh <160146755+godunguh@users.noreply.github.com>
2026-04-18 10:04:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import streamlit as st
 st.title("카페")
+
+places = [
+    {"이름": "칠성조선소", "지역": "속초", "실내여부": "실내외", "평점": 4.4, "대표메뉴": "메이플 마롱 밀크"},
+    {"이름": "감자밭", "지역": "춘천", "실내여부": "실내외", "평점": 4.5, "대표메뉴": "강원도 감자빵"},
+    {"이름": "커피공장", "지역": "강릉", "실내여부": "실내외", "평점": 4.6, "대표메뉴": "핸드드립 커피"},
+    {"이름": "갤러리밥스", "지역": "강릉", "실내여부": "실내", "평점": 4.5, "대표메뉴": "초옥이커피"}
+]
+
+def get_recommendations(data, region, indoor):
+    result = []
+    for place in data:
+        if place["지역"] == region and place["실내여부"] == indoor:
+            result.append(place)
+    return result
