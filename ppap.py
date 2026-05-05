0a04313f1936824237c34542a843dd9c0f2c1465
godunguh <160146755+godunguh@users.noreply.github.com>
2026-05-05 10:21:24 +0900

    Update ppap.py

--- ppap.py
+++ ppap.py
@@ -31,3 +31,18 @@ if len(result) > 0:
     st.dataframe(result)
 else:
     st.warning("조건에 맞는 장소가 없습니다.")
+
+region_count = df["지역"].value_counts()
+
+st.subheader("지역별 장소 개수")
+st.bar_chart(region_count)
+
+type_count = df["유형"].value_counts()
+
+st.subheader("유형별 장소 개수")
+st.bar_chart(type_count)
+
+avg_score = df.groupby("지역")["평점"].mean()
+
+st.subheader("지역별 평균 평점")
+st.bar_chart(avg_score)
