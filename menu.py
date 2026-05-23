85dc84ff3ecbedbd7efdd126acc83d3c74f45aca
godunguh <160146755+godunguh@users.noreply.github.com>
2026-05-23 09:37:31 +0900

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 import streamlit as st
 menu = st.sidebar.radio("원하는 기능 선택", ["데이터 확인", "조건검색"])
 
-if menu == "데이터 확인"
+if menu == "데이터 확인" :
   def filter_places():
     if len(result) > 0:
         st.dataframe(result)
