3493d788b135b4cc06fc659e0ee49c67779f9118
godunguh <160146755+godunguh@users.noreply.github.com>
2026-05-23 10:01:49 +0900

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-def load_data(df):
+def load_data():
     uploaded_file = st.file_uploader(
         "장소 데이터 엑셀 파일을 업로드하세요",
         type=["xlsx"]
@@ -15,7 +15,7 @@ def load_data(df):
     else:
         st.info("엑셀 파일을 업로드하면 데이터가 표시됩니다.")
 
-def get_user_input(df):
+def get_user_input():
     selected_region = st.selectbox("지역을 선택하세요", df["지역"].unique())
     selected_budget = st.number_input("사용 가능한 예산을 입력하세요", min_value=0, value=10000, step=1000)
 
@@ -25,7 +25,7 @@ def get_user_input(df):
     ]
 
 
-def filter_places(df):
+def filter_places():
     if len(result) > 0:
         st.dataframe(result)
     else:
@@ -33,7 +33,7 @@ def filter_places(df):
 
     region_count = df["지역"].value_counts()
 
-def show_charts(df):
+def show_charts():
     st.subheader("지역별 장소 개수")
     st.bar_chart(region_count)
 
@@ -50,12 +50,12 @@ def show_charts(df):
 menu = st.sidebar.radio("원하는 기능 선택", ["엑셀 입력", "데이터 확인", "조건검색"])
 
 if menu == "엑셀 입력" :
-    load_data(df)
+    load_data()
     
 elif menu == "데이터 확인" :
-    show_charts(df)
+    show_charts()
 
 elif menu == "조건검색" :
-    get_user_input(df)
-    filter_places(df)
+    get_user_input()
+    filter_places()
 
